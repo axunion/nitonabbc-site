af2d92514df97a44dfd8c2440023d69e0e042377
axunion <120321953+axunion@users.noreply.github.com>
2024-03-07 00:48:56 +0900

    Define site data as constants

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_URL = "https://nitonabbc.org";
+export const SITE_TITLE = "仁戸名聖書バプテスト教会";
+export const SITE_DESCRIPTION =
+  "仁戸名聖書バプテスト教会は、1963年に創設以来、60年以上にわたり地域の方々にキリストの愛をお伝えしてきた教会です。";
