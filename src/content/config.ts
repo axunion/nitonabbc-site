15f5c51956cb26b22122a2e6499bc5d24978d4b6
axunion <120321953+axunion@users.noreply.github.com>
2024-03-09 15:55:07 +0900

    Add specials page and config

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,15 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
-const blog = defineCollection({
-	type: 'content',
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-	}),
+const specials = defineCollection({
+  type: "content",
+  schema: ({ image }) =>
+    z.object({
+      date: z.coerce.date(),
+      title: z.string(),
+      description: z.string().optional(),
+      tags: z.array(z.string()).optional(),
+      cover: image(),
+    }),
 });
 
-export const collections = { blog };
+export const collections = { specials };
