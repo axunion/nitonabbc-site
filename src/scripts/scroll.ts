1ee7986fb0eb639d45a3aeb499d3704bad0c0c21
axunion <120321953+axunion@users.noreply.github.com>
2024-04-24 01:26:51 +0900

    Refine IntersectionObserver

--- src/scripts/scroll.ts
+++ src/scripts/scroll.ts
@@ -1,27 +1,24 @@
 const scrollTargets = document.querySelectorAll("[data-scroll]");
-const viewportThreshold = window.innerHeight * 0.5;
 
 const observer = new IntersectionObserver(
   (entries) => {
     entries.forEach((entry) => {
-      if (entry.target instanceof HTMLElement) {
+      if (entry.isIntersecting && entry.target instanceof HTMLElement) {
         const className = entry.target.dataset.scroll;
 
         if (className) {
-          if (entry.intersectionRect.height >= viewportThreshold) {
-            document.body.classList.forEach((cls) => {
-              if (cls.indexOf("theme-") === 0) {
-                document.body.classList.remove(cls);
-              }
-            });
+          document.body.classList.forEach((cls) => {
+            if (cls.indexOf("theme-") === 0) {
+              document.body.classList.remove(cls);
+            }
+          });
 
-            document.body.classList.add(className);
-          }
+          document.body.classList.add(className);
         }
       }
     });
   },
-  { threshold: [0.1, 0.2, 0.3, 0.4, 0.5] }
+  { rootMargin: "-50% 0px" }
 );
 
 scrollTargets.forEach((target) => {
