261c78355ba7f71736189a9f7ad7628912d0e74f
axunion <120321953+axunion@users.noreply.github.com>
2024-11-26 23:39:01 +0900

    Refactor descriptions to reflect page-specific details

--- src/consts.ts
+++ src/consts.ts
@@ -4,4 +4,4 @@
 export const SITE_URL = "https://nitonabbc.org";
 export const SITE_TITLE = "仁戸名聖書バプテスト教会";
 export const SITE_DESCRIPTION =
-  "仁戸名聖書バプテスト教会は、1963年に創設以来、60年以上にわたり地域の方々にキリストの愛をお伝えしてきた教会です。";
+  "仁戸名聖書バプテスト教会は、1963年の創設以来、60年以上にわたり地域の皆さまとともに歩み、キリストの愛と希望を分かち合ってきた教会です。";
